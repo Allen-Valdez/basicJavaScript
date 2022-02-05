0cf86f4d5e2efb1bd4a675682b9bfa69f3aea834
Allen Valdez <AllenValdez1728@gmail.com>
2022-02-05 15:54:06 -0800

    Learned about data types and what they do

---
+++ myLearning/Variables/dataTypes.js
@@ -0,0 +1,25 @@
+// In JavaScript we have 6 different data types
+
+/* 1.) Strings: 
+          - Strings are sequences of characters which can be numbers, letters, symbols, and honestly whatever you want as long as its wrapped in quotes.Either single or double quotes.
+*/
+let string = "Hello! This is a string";
+/* 2.) Number:
+          - Numbers is literally just a number! In JavaScript even ints, doubles, floats, etc are all considered numbers
+ */
+let number = 5;
+/* 3.) Boolean:
+          - Booleans is just true or false!
+*/
+let isTrue = true;
+/* 4.) Null:
+          - Null is an intentional empty value. 
+*/
+let leftNull = null;
+/* 5.) Undefined:
+          - Undefined is a variables that has NOT been assigned a value
+*/
+let undefinedVariable;
+/* 6.) Symbols (ES6)
+
+ */
